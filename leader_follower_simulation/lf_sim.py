4e68dcdc269e398b4e6e871eee879fdd3a004a14
Jimmy Fernandez <jfern117@gmail.com>
2025-02-19 21:09:57 -0500

    minor progress, currently stuck on theoretical details

---
+++ leader_follower_simulation/lf_sim.py
@@ -0,0 +1,11 @@
+import numpy as np
+import networkx as nx
+import scipy.integrate
+import matplotlib.pyplot as plt
+import scipy.signal
+
+#suppose we have N + 1 vehicles in a 2D plane with second order integrator dynamics (p = v, vdot = u)
+
+
+
+
