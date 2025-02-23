87e3d8ea89cf5f2a778f46c746958082b7930292
Jimmy Fernandez <jfern117@gmail.com>
2025-02-23 01:31:41 -0500

    added todos as an outline

--- leader_follower_simulation/lf_sim.py
+++ leader_follower_simulation/lf_sim.py
@@ -6,6 +6,14 @@ import scipy.signal
 
 #suppose we have N + 1 vehicles in a 2D plane with second order integrator dynamics (p = v, vdot = u)
 
+#TODO: setup network
+#TODO: set up init config
+#TODO: setup leader (linear constant velocity to start)
+#TODO: set offsets
+#TODO: code up consensus sum
+#TODO: setup RK45 for all agents
+#TODO: setup plot and visualize
+#TODO: vary and test
 
 
 
